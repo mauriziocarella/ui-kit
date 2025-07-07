99da2d623aef20909e9143f2dd307238217d7a42
Maurizio Carella <maurizio.carella27@gmail.com>
2025-07-07 02:03:36 +0200

    chore: remove Storybook ESLint plugin configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import {defineConfig, globalIgnores} from 'eslint/config';
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
